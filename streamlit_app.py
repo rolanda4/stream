83b04f23446bce99a7ab1b64c5a809b11b3a3c5a
Rolanda Azeem <35231659+rolanda4@users.noreply.github.com>
2024-11-08 11:05:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,6 @@ with st.expander('Input'):
   st.write('**Your Entries**')
   input_df
 
-
-
 #encoding categorical values to become numerals
 df_encoded = pd.get_dummies(input_parameters, columns=['Home', 'Intent'])
 
@@ -86,6 +84,10 @@ with st.expander('Data preparation'):
   
 # Model training and inference
 ## Train the ML model
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
 
 
 
