9c36e0bf46f4c0234e2581960778f107263efd60
Rolanda Azeem <35231659+rolanda4@users.noreply.github.com>
2024-11-11 10:37:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,19 +62,12 @@ X = df_encoded[1:]
 input_row = df_encoded[:1]
 
 # Encode y
-#y= np.argmax(df_encoded[['Default_N', 'Default_Y']].values, axis=1)
-
 target_mapper = {'N': 0,
                  'Y': 1,}
 def target_encode(val):
   return target_mapper[val]
 
 y = y_raw.apply(target_encode)
-
-#applying scalar to normalize
-scaler = StandardScaler()
-scaled_features = scaler.fit_transform(df_encoded)
-df_scaled = pd.DataFrame(scaled_features, columns=df_encoded.columns)
 
 with st.expander('Data preparation'):
   st.write('**Encoded X (input data)**')
@@ -85,6 +78,12 @@ with st.expander('Data preparation'):
 # Model training and inference
 ## Train the ML model
 X_train, X_test, y_train, y_test = train_test_split(X_raw, y, test_size=0.2, random_state=42)
+
+#applying scalar to normalize
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test )
+input_row_scaled = scaler.transform(input_row)
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
